d718b9317233e7efbf078d21b52572063d1176cf
Dhwani Suthar <20ce140@charusat.edu.in>
2022-08-14 14:52:56 +0530

    Register User Controller started

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,12 @@ app.use(express.json());
 
 // Route imports
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
 
 app.use("/api/v1",product);
+app.use("/api/v1/users",user);
+
+// Middleware to handle errors
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+// Register a User
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a sample id",
+      url: "profilepicURL",
+    },
+  });
+  
+  res.status(201).json({
+    success: true,
+    user,
+  });
+});
