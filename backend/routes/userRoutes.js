188abe6b69379642940ce97e307fcfe966ed787c
Dhwani Suthar <20ce140@charusat.edu.in>
2022-09-12 22:49:51 +0530

    Update User Password - controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require("../controllers/userController");
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword } = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").post(logout);
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser,updatePassword)
 module.exports = router;
 
