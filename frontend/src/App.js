7949d9623737fce67915aa842f1b57c6e711d7f5
Dhwani Suthar <20ce140@charusat.edu.in>
2022-09-18 21:57:51 +0530

    Header Categories and CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,29 @@ import Header from "./component/layout/Header/Header.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebFont from "webfontloader";
 import React from "react";
+import Home from "./component/Home/Home";
 import store from "./store";
+import { useSelector } from "react-redux";
 
 function App() {
 
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
 
-    useEffect(() => {
-        WebFont.load({
-            google: {
-                families: ["Roboto", "Droid Sans", "Chilanka"],
-            },
-        });
+  }, []);
 
-    }, []);
-
-    return (
-        <Router>
-            <Header />
-            <Switch>
-                {/* Routes here */}
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Fragment, useEffect } from "react";
+import "./Home.css";
+import MetaData from "../layout/MetaData";
+
+
+const Home = () => {
+  
+  return (
+    
+    <Fragment>
+   <MetaData title="ECOMMERCE & Dashboard" />
+    </Fragment >
+  );
+};
+
+export default Home;
