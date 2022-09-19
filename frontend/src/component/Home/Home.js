41d94ab1cab0819b8b8e8082244141a0af589d25
Dhwani Suthar <20ce140@charusat.edu.in>
2022-09-19 19:34:47 +0530

    Home - Banner and Categories

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -4,83 +4,180 @@ import MetaData from "../layout/MetaData";
 
 
 const Home = () => {
-  
+
   return (
-    
+
     <Fragment>
-   <MetaData title="ECOMMERCE & Dashboard" />
-   Home {/* ------------------ Banner ---------------------- */}
-            <div class="banner">
+      <MetaData title="ECOMMERCE & Dashboard" />
+      {/* ------------------ Banner ---------------------- */}
+      <div class="banner">
 
-              <div class="container">
+        <div class="container">
 
-                <div class="slider-container has-scrollbar">
+          <div class="slider-container has-scrollbar">
 
-                  <div class="slider-item">
+            <div class="slider-item">
 
-                    <img src="../../../images/banner1.png" alt="women's latest fashion sale" class="banner-img" />
+              <img src="../../../images/banner1.png" alt="women's latest fashion sale" class="banner-img" />
 
-                    <div class="banner-content">
+              <div class="banner-content">
 
-                      <p class="banner-subtitle">New Products Every Week!</p>
+                <p class="banner-subtitle">New Products Every Week!</p>
 
-                      <h2 class="banner-title">Women's latest fashion</h2>
+                <h2 class="banner-title">Women's latest fashion</h2>
 
-                      <p class="banner-text">
-                        at the best price
-                      </p>
+                <p class="banner-text">
+                  at the best price
+                </p>
 
-                      <a href="/products" class="banner-btn">Shop now</a>
+                <a href="/products" class="banner-btn">Shop now</a>
 
-                    </div>
+              </div>
 
+            </div>
+
+            <div class="slider-item">
+
+              <img src="../../../images/banner2.png" alt="modern sunglasses" class="banner-img" />
+
+              <div class="banner-content">
+
+                <p class="banner-subtitle">Trending accessories</p>
+
+                <h2 class="banner-title">Vast Collection</h2>
+
+                <p class="banner-text">
+                  starting at just <b>30</b>/-
+                </p>
+
+                <a href="/products" class="banner-btn">Shop now</a>
+
+              </div>
+
+            </div>
+
+            <div class="slider-item">
+
+              <img src="../../../images/banner3.png" alt="new fashion summer sale" class="banner-img" />
+
+              <div class="banner-content">
+
+                <p class="banner-subtitle">Happy Customers</p>
+
+                <h2 class="banner-title">Read what people have to say about us!</h2>
+
+                <p class="banner-text">
+                  Read here
+                </p>
+
+                <a href="/about" class="banner-btn">Testimoties</a>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* ---------------- Category ------------------- */}
+
+        <div class="category">
+
+          <div class="container">
+
+            <div class="category-item-container has-scrollbar">
+
+              <div class="category-item">
+
+                <div class="category-img-box">
+                  <img src="../../../images/icons/dress.svg" alt="frock" width="30" />
+                </div>
+
+                <div class="category-content-box">
+
+                  <div class="category-content-flex">
+                    <h3 class="category-item-title">Dress & frock</h3>
+
+                    {/* <p class="category-item-amount">(53)</p> */}
                   </div>
 
-                  <div class="slider-item">
+                  <a href="/products/frock" class="category-btn">Show all</a>
 
-                    <img src="../../../images/banner2.png" alt="modern sunglasses" class="banner-img" />
+                </div>
 
-                    <div class="banner-content">
+              </div>
 
-                      <p class="banner-subtitle">Trending accessories</p>
+              <div class="category-item">
 
-                      <h2 class="banner-title">Vast Collection</h2>
+                <div class="category-img-box">
+                  <img src="../../../images/icons/shorts.svg" alt="shorts & jeans" width="30" />
+                </div>
 
-                      <p class="banner-text">
-                        starting at just <b>30</b>/-
-                      </p>
+                <div class="category-content-box">
 
-                      <a href="/products" class="banner-btn">Shop now</a>
+                  <div class="category-content-flex">
+                    <h3 class="category-item-title">Shorts & jeans</h3>
 
-                    </div>
-
+                    {/* <p class="category-item-amount">(84)</p> */}
                   </div>
 
-                  <div class="slider-item">
+                  <a href="/products/jeans" class="category-btn">Show all</a>
 
-                    <img src="../../../images/banner3.png" alt="new fashion summer sale" class="banner-img" />
+                </div>
 
-                    <div class="banner-content">
+              </div>
 
-                      <p class="banner-subtitle">Happy Customers</p>
+              <div class="category-item">
 
-                      <h2 class="banner-title">Read what people have to say about us!</h2>
+                <div class="category-img-box">
+                  <img src="../../../images/icons/tee.svg" alt="t-shirts" width="30" />
+                </div>
 
-                      <p class="banner-text">
-                        Read here
-                      </p>
+                <div class="category-content-box">
 
-                      <a href="/about" class="banner-btn">Testimoties</a>
+                  <div class="category-content-flex">
+                    <h3 class="category-item-title">T-shirts</h3>
 
-                    </div>
-
+                    {/* <p class="category-item-amount">(35)</p> */}
                   </div>
+
+                  <a href="/products/tshirt" class="category-btn">Show all</a>
+
+                </div>
+
+              </div>
+
+              <div class="category-item">
+
+                <div class="category-img-box">
+                  {/* <a target="_blank" href="https://icons8.com/icon/Euh7KJO4xRh6/accesory">Accesory</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
+                  <img src="../../../images/icons/accessories.png" alt="jacket" width="30" />
+                </div>
+
+                <div class="category-content-box">
+
+                  <div class="category-content-flex">
+                    <h3 class="category-item-title">Accessories</h3>
+
+                    {/* <p class="category-item-amount">(16)</p> */}
+                  </div>
+
+                  <a href="/products/earrings" class="category-btn">Show all</a>
 
                 </div>
 
               </div>
 
             </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+
     </Fragment >
   );
 };
