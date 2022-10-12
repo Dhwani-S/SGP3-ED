78416ec3aa535cf542b57cdbb798c1b57ebfcce3
kavya-thaker <20ce146@charusat.edu.in>
2022-10-12 12:21:58 +0530

    faqs

--- frontend/src/component/layout/FAQ/faq.js
+++ frontend/src/component/layout/FAQ/faq.js
@@ -5,27 +5,29 @@ import './faq.css';
 const Faq = () => {
   const accordionData = [
     {
-      title: 'question1',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-      laborum cupiditate possimus labore, hic temporibus velit dicta earum
-      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-      voluptatem.`
+      title: 'Do I need to create an account to place an order?',
+      content: `Yes, you need to create an account to place any orders with us.
+       It’s a way to provide users easy access to their order history.`
     },
     {
-      title: 'question2',
-      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
-      reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
-      quaerat iure quos dolorum accusantium ducimus in illum vero commodi
-      pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
-      quidem maiores doloremque est numquam praesentium eos voluptatem amet!
-      Repudiandae, mollitia id reprehenderit a ab odit!`
+      title: 'What if I need to change my order after I hit “SUBMIT”?',
+      content: `Once your order is placed, we start working on it right away. Unfortunately, this means we can’t make changes to an order once its placed. 
+      In limited circumstances, we may be able to cancel orders if they have not shipped. Please contact us for assistance.`
     },
     {
-      title: 'question3',
-      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-      dolor ut sequi minus iste? Quas?`
-    }
+      title: 'What do I do if something in my order is damaged or missing?',
+      content: `In case you have received a damaged product or any ordered item is missing, kindly contact us. You can contact us by email(….@xyz.com) or call us(978xxxxxxx). 
+      Please remember to include the order number, full shipping address and the product that is damaged/missing.`
+    },
+    {
+      title: 'What is your return policy?',
+      content: `You can return all purchases up to 30 days from the date you receive them.`
+    },
+    {
+      title: 'What information do I need to checkout?',
+      content: `While checking-out you will need to provide your full address and contact information and make a payment through the provided payment gateway. 
+      Before proceeding for the payment please make sure your address and contact information provided is correct.`
+    },
   ];
 
   return (
