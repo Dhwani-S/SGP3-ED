92e9292bbed4eddf7489fd4321c53bfc1d9e78e9
kavya-thaker <20ce146@charusat.edu.in>
2022-10-19 14:49:14 +0530

    proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import { useEffect } from "react";
+import React, { useState } from 'react';
 import Header from "./component/layout/Header/Header.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebFont from "webfontloader";
-import React from "react";
+// import React from "react";
 import Footer from "./component/layout/Footer/Footer";
 import Home from "./component/Home/Home";
 import ProductDetails from "./component/Product/ProductDetails";
